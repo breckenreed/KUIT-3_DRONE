7c6a66cbd3340e62dc3517098da17cce20aea5c1
breckenreed <62158298+breckenreed@users.noreply.github.com>
2023-06-14 16:27:22 +0300

    Create opener.cpp

---
+++ opener.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "OMGAWS Lambda provides a convenient way to run your code in a serverless manner, eliminating the need for server management, and allowing you to focus on writing and deploying your application logic." << std::endl;
+    return 0;
+}
